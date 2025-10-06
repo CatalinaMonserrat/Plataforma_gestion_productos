80b692f1cd046c5821dddbf936c1925d7a5d0165
CatalinaMonserrat <kata1800@gmail.com>
2025-10-06 20:16:39 -0300

    Se trabaja en la clase dentro de models.py y se crean grupos y usuarios a traves de http://127.0.0.1:8000/admin/

--- gestor/producto/admin.py
+++ gestor/producto/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Producto
 
-# Register your models here.
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'stock', 'creado_en')
+    list_filter = ('creado_en',)
+    search_fields = ('nombre', 'descripcion')
+    ordering = ('-creado_en',)
+
+# Solo el superusuario puede eliminar desde el admin:
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
--- gestor/producto/models.py
+++ gestor/producto/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=120)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-creado_en',)
+
+    def __str__(self):
+        return f"{self.nombre} | {self.precio}"
